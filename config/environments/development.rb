a3af32e1c6d729f4cd291ed336c683fcdaf29056
oscarjes <oscar@jesionek.co>
2017-08-18 12:21:02 +0700

    added production host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   Rails.application.routes.default_url_options[:host] = 'localhost:3000' 
-   
+  
   
 end

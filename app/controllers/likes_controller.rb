2c00ebf74eb277542f2650be82439e002d4b9233
oscarjes <oscar@jesionek.co>
2017-08-18 16:27:14 +0700

    ajax liking of wallposts and comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,18 @@
 class LikesController < ApplicationController
   def toggle
     if params[:wall_post_id]
-      item = WallPost.find params[:wall_post_id]
+      @item = WallPost.find params[:wall_post_id]
     elsif params[:comment_id]
-      item = Comment.find params[:comment_id]
+      @item = Comment.find params[:comment_id]
     end
-    current_user.likes_toggle!(item)
-    redirect_back fallback_location: root_path
+    current_user.likes_toggle!(@item)
+
+    respond_to do |format|
+      format.html {
+        redirect_back fallback_location: root_path
+      }
+
+      format.js
+    end
   end
 end

ebef05bfdb3d80497fcb5db6d6dc6d9ce5ced2a3
oscarjes <oscar@jesionek.co>
2017-08-23 17:13:40 +0700

    precompile heroku 3

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,13 @@ module Minimalnet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.initialize_on_precompile = false    
+    
     
     ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
